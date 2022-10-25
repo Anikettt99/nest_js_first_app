2a62c0f1f020f0990bbcc11783f619abf17ae17a
Aniket <aniketsinha99123490@gmail.com>
2022-10-25 20:35:20 +0530

    passport_authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { UserService } from 'src/user/user.service';
+import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private userService: UserService) {}
+  constructor(private authService: AuthService) {}
 
+  @UseGuards(AuthGuard('local'))
   @Post('/login')
-  async login(@Body() loginDto: any) {
-    const user = await this.userService.findByEmail(loginDto.email);
-
-    if (user) {
-      if (user.password === loginDto.password) {
-        return user;
-      }
-
-      return 'wrong password';
-    }
-    return 'unauthenticated';
+  async login(@Request() req: any) {
+    // passport is inserting this user in req object
+    return req.user;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './login.strategy';
 
 @Module({
   controllers: [AuthController],
-  imports: [UserModule], // we are only importing that is exporting by userModule i.e UserService
+  imports: [UserModule, PassportModule],
+  providers: [AuthService, LocalStrategy], // we are only importing that is exporting by userModule i.e UserService
 })
 export class AuthModule {}
